a84798ba1aefa211e6fecdc5b80dde34ce599a82
tngusiam <145095583+tngusiam@users.noreply.github.com>
2023-09-21 17:55:56 +0900

    Add files via upload

---
+++ js/submenu2.js
@@ -0,0 +1,17 @@
+$(".submenu").hide();
+$(".gnb li").mouseenter(function(){
+    $(".submenu").stop().slideDown(300)
+});
+$(".header").mouseleave(function(){
+    $(".submenu").stop().slideUp(300)
+});
+$(window).scroll(function(){
+    if($(this).scrollTop() > 100){
+        $('.header').addClass('header_scroll')
+    }else{
+        $('.header').removeClass('header_scroll')
+    }
+})
+$("#board> li> a").click(function(){
+    $(this).parent().addClass("on").siblings().removeClass("on")
+});
