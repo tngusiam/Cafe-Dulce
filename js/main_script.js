44f1494b0512d83d3f66f8d108ab05fa3d6bbf74
tngusiam <145095583+tngusiam@users.noreply.github.com>
2023-09-21 14:29:35 +0900

    Add files via upload

---
+++ js/main_script.js
@@ -0,0 +1,15 @@
+$(".submenu").hide();
+$(".gnb li").mouseenter(function(){
+    $(".submenu").stop().slideDown(300)
+});
+$(".header").mouseleave(function(){
+    $(".submenu").stop().slideUp(300)
+});
+$(window).scroll(function(){
+    if($(this).scrollTop() > 100){
+        $('.header').addClass('header_scroll')
+    }else{
+        $('.header').removeClass('header_scroll')
+    }
+})
+
